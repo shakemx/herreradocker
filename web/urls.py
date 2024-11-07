7b39adccc09b938ffc22db187d869cf2b06397a0
Guillermo Pineda <guillermopineda@MinideGuillermo.bbrouter>
2024-11-06 20:43:01 -0600

    reload actions

--- web/urls.py
+++ web/urls.py
@@ -8,4 +8,4 @@ urlpatterns = [
     path('portafolio/<slug:slug_type>', insurance, name='insurance'),
     path('portafolio/<slug:slug_type>/<slug:slug_category>', insurances, name='insurances'),
     path('portafolio/<slug:slug_type>/<slug:slug_category>/<slug:slug_product>', product, name='product'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
